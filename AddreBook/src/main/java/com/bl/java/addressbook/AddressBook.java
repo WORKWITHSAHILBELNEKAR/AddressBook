13a7f68d36aee48fd6ec558f38d813c82a5c445d
sahil belnekar <sahilsbelnekar2002@gmail.com>
2024-09-15 12:10:23 +0530

    create contact address

--- AddreBook/src/main/java/com/bl/java/addressbook/AddressBook.java
+++ AddreBook/src/main/java/com/bl/java/addressbook/AddressBook.java
@@ -1,7 +1,25 @@
 package com.bl.java.addressbook;
 
+import java.util.*;
 public class AddressBook {
-    public static void main(String[] args) {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
 
+
+    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 }
